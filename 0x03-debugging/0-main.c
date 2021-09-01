6e2c63bd853a9eeb8149b307e2b55e35a6d7bc1a
enabebe <nathabe984@gmail.com>
2021-09-01 16:12:31 +0300

    0-main.c Added

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,15 @@
-#include "main.h"
+  
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
 
-/**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
+/* betty style doc for function main goes there */
 int main(void)
 {
-	int i;
+	int n;
 
-		i = 98; 
-		positive_or_negative(i);
-
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
 	return (0);
 }
