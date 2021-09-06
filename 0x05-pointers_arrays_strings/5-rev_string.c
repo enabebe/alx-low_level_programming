ce67b1d2bbf13df9d15133c06fff82cf84a5be93
enabebe <nathabe984@gmail.com>
2021-09-06 11:47:12 +0300

    5-rev_string.c added

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+    char s[10] = "My School";
+
+    printf("%s\n", s);
+    rev_string(s);
+    printf("%s\n", s);
+    return (0);
+}
