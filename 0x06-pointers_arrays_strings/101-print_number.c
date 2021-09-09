a4677fc57029a4a37a232bfb7e655fe898c5a646
enabebe <nathabe984@gmail.com>
2021-09-09 19:51:33 +0300

    101-print_number.c file added

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,17 +6,18 @@
  */
 void print_number(int n)
 {
-    // If number is smaller than 0, put a - sign
-    // and change number to positive
-	if (n < 0) {
-		putchar('-');
-		n = -n;
-    }
-  
-    // Remove the last digit and recur
-	if (n/10)
-		print(n/10);
-  
-    // Print the last digit
-	putchar(n%10 + '0');
+ 	  if (n < 0)
+        {
+                _putchar(0x2D);
+                n = -n;
+        }
+
+        if (n > 9)
+        {
+                _put_nbr(n/10);
+        }
+
+        _putchar((n%10) + '0');
+
+        return 0;
 }
