045dca18cb851b1257652437fbf4ebc2d0ab20bb
enabebe <nathabe984@gmail.com>
2021-09-09 18:55:31 +0300

    101-print_number.c file added

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,8 +14,9 @@ void print_number(int n)
 		num = -num;
 	}
 
-	if ((num / 10) > 0)
+	else if ((num / 10) > 0)
+	{
 		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
+		_putchar((num % 10) + '0');
+	}
 }
