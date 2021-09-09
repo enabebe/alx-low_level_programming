816ca5868840d0f7e4389cb05862c3b2600ef8ce
enabebe <nathabe984@gmail.com>
2021-09-09 21:50:07 +0300

    101-print_number.c file added

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,20 +9,14 @@
  */
 void print_number(int n)
 {
-	unsigned int num;
-/*check if number is negative*/
 
-	num = n;
-
-	if (n < 0)
+	if (n < 0) 
 	{
-		_putchar(45);
-		num = -n;
-	}
-/* print number by recursion*/
-	if (num / 10)
-	{
-		print_number(num / 10);
-	}
-	_putchar((num % 10) + '0');
+        	putchar('-');
+        	n = -n;
+    	}
+    	if (n / 10 != 0)
+        	print_number(n / 10);
+    
+	putchar((n % 10) + '0');
 }
