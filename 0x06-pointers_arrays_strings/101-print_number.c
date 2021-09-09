7ee8c1abdbd15a92fbc8daf8df06873e0f492aee
enabebe <nathabe984@gmail.com>
2021-09-09 20:17:43 +0300

    101-print_number.c file added

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,28 +1,26 @@
-#include <stdio.h>
 #include "main.h" 
+/**
+ *print_number - prints an integer.
+ *only using the putchar function.
+ *noarrays and pointers.
+ *@n: integer to be printed.
+ *
+ *Return: void.
+ */
 void print_number(int n)
 {
-    // If number is smaller than 0, put a - sign
-    // and change number to positive
+   	 // If number is smaller than 0, put a - sign 
+    	// and change number to positive 
 	if (n < 0) 
-	{
-        	putchar('-');
-        	n = -n;
-    	}
+	{ 
+        	putchar('-'); 
+        	n = -n; 
+	} 
   
-    // Remove the last digit and recur
-    	if (n/10)
-        	print(n/10);
+    // Remove the last digit and recur 
+    	if (n/10) 
+        	print_number(n/10); 
   
-    // Print the last digit
-    	putchar(n%10 + '0');
-}
-  
-// Driver program to test abvoe function
-int main(void)
-{
-    	int n = 12045;
-    
-    		print(n);
-    	return 0;
+    // Print the last digit 
+    	putchar(n%10 + '0'); 
 }
