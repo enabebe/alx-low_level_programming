598aea5e869f69d0885372f3a5173b6bd702699b
enabebe <nathabe984@gmail.com>
2021-09-17 13:43:58 +0300

    100-change.c file added

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,43 +1,26 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
 
 /**
- * main - prints the minimum number of coins to
- * make change for an amount of money
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - prints minimum number of coins to make change for an amount of money.
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
  *
- * Return: 0 (Success), 1 (Error)
+ * Return: 0 if no errors, else 1
  */
 int main(int argc, char *argv[])
 {
-	int num, j, result;
-	int coins[] = {25, 10, 5, 2, 1};
+	int a, n = 0, i, t;
+	int c[5] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
-		printf("Error\n");
+		puts("Error");
 		return (1);
 	}
-
-	num = atoi(argv[1]);
-	result = 0;
-
-	if (num < 0)
+	a = atoi(argv[1]);
+	if (a <= 0)
 	{
-		printf("0\n");
-		return (0);
+		puts("0");
+		return (1);
 	}
-
-	for (j = 0; j < 5 && num >= 0; j++)
-	{
-		while (num >= coins[j])
-		{
-			result++;
-			num -= coins[j];
-		}
-	}
-
-	printf("%d\n", result);
-	return (0);
-}
